3a14dd803fc66c005d7f08dc0936f49473785468
Malleboyina <manmadhamalleboyina91024@gmail.com>
2022-07-13 11:27:44 +0530

    final commit

---
+++ shop-api/src/main/java/eshop/homedecor/shopapi/api/CartController.java
@@ -0,0 +1,100 @@
+package eshop.homedecor.shopapi.api;
+
+
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import eshop.homedecor.shopapi.entity.Cart;
+import eshop.homedecor.shopapi.entity.ProductInOrder;
+import eshop.homedecor.shopapi.entity.ProductInfo;
+import eshop.homedecor.shopapi.entity.User;
+import eshop.homedecor.shopapi.form.ItemForm;
+import eshop.homedecor.shopapi.repository.ProductInOrderRepository;
+import eshop.homedecor.shopapi.service.CartService;
+import eshop.homedecor.shopapi.service.ProductInOrderService;
+import eshop.homedecor.shopapi.service.ProductService;
+import eshop.homedecor.shopapi.service.UserService;
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+    @Autowired
+    CartService cartService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    ProductService productService;
+    @Autowired
+    ProductInOrderService productInOrderService;
+    @Autowired
+    ProductInOrderRepository productInOrderRepository;
+
+    @PostMapping("")
+    public ResponseEntity<Cart> mergeCart(@RequestBody Collection<ProductInOrder> productInOrders, Principal principal) {
+        User user = userService.findOne(principal.getName());
+        try {
+        	//hello
+            cartService.mergeLocalCart(productInOrders, user);
+        } catch (Exception e) {
+            ResponseEntity.badRequest().body("Merge Cart Failed");
+        }
+        return ResponseEntity.ok(cartService.getCart(user));
+    }
+
+    @GetMapping("")
+    public Cart getCart(Principal principal) {
+        User user = userService.findOne(principal.getName());
+        return cartService.getCart(user);
+    }
+
+
+    @PostMapping("/add")
+    public boolean addToCart(@RequestBody ItemForm form, Principal principal) {
+    	ProductInfo productInfo = productService.findOne(form.getProductId());
+        try {
+            mergeCart(Collections.singleton(new ProductInOrder(productInfo, form.getQuantity())), principal);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    @PutMapping("/{itemId}")
+    public ProductInOrder modifyItem(@PathVariable("itemId") String itemId, @RequestBody Integer quantity, Principal principal) {
+        User user = userService.findOne(principal.getName());
+         productInOrderService.update(itemId, quantity, user);
+        return productInOrderService.findOne(itemId, user);
+    }
+
+    @DeleteMapping("/{itemId}")
+    public void deleteItem(@PathVariable("itemId") String itemId, Principal principal) {
+        User user = userService.findOne(principal.getName());
+         cartService.delete(itemId, user);
+         // flush memory into DB
+    }
+
+
+    @PostMapping("/checkout")
+    public ResponseEntity checkout(Principal principal) {
+        User user = userService.findOne(principal.getName());// Email as username
+        cartService.checkout(user);
+        return ResponseEntity.ok(null);
+    }
+
+
+}
